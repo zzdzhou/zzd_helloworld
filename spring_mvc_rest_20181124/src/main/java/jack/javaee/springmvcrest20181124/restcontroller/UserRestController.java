545fd0fd1239940684bf14d4db34c61558cd81d2
zzdzhou <zzd16000@163.com>
2018-11-29 00:47:08 +0000

    spring mvc rest

---
+++ spring_mvc_rest_20181124/src/main/java/jack/javaee/springmvcrest20181124/restcontroller/UserRestController.java
@@ -0,0 +1,25 @@
+package jack.javaee.springmvcrest20181124.restcontroller;
+
+import jack.javaee.springmvcrest20181124.domain.User;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2018-11-29
+ */
+@RestController
+@RequestMapping("/user")
+public class UserRestController {
+
+    @RequestMapping("/echo")
+    public User user(@RequestBody User user) {
+        return user;
+    }
+
+}
