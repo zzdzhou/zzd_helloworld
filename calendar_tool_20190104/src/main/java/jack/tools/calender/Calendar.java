37746d30d5425c0b217694aeba58166a60a6ae33
zzdzhou <zhengde.zhou@outlook.com>
2019-01-04 19:27:25 +0800

    calendar_tool

---
+++ calendar_tool_20190104/src/main/java/jack/tools/calender/Calendar.java
@@ -0,0 +1,19 @@
+package jack.tools.calender;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2019-01-04
+ */
+public class Calendar {
+
+    private int id;
+    private String country;
+
+    private String brand;
+    private String calendarCode;
+
+}
---
+++ calendar_tool_20190104/src/main/java/jack/tools/calender/CalendarTool.java
@@ -0,0 +1,13 @@
+package jack.tools.calender;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2019-01-04
+ */
+public class CalendarTool {
+
+}
