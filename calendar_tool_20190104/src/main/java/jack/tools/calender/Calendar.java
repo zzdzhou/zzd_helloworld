66f8fde495742ccf9c0cde29079edb588319b344
zzdzhou <zhengde.zhou@outlook.com>
2019-01-07 19:05:47 +0800

    20190107

--- calendar_tool_20190104/src/main/java/jack/tools/calender/Calendar.java
+++ calendar_tool_20190104/src/main/java/jack/tools/calender/Calendar.java
@@ -1,5 +1,7 @@
 package jack.tools.calender;
 
+import java.time.LocalDate;
+
 /**
  * Theme:
  * <p>
@@ -10,10 +12,88 @@ package jack.tools.calender;
  */
 public class Calendar {
 
-    private int id;
+    public static final String INSERT_PREFIX = "BEGIN;\n\nINSERT INTO `calendar` (`COUNTRY`, `BRAND`, `CALENDAR_CODE`, `YEAR`," +
+            " `TOTAL_WORKING_DAYS`, `TOTAL_NON_WORKING_DAYS`, `MONTH`, `YEAR_MONTH_DAY`, `NON_WORKING_DAY_FLAG`, " +
+            "`WORKING_DAY_NUMBER`, `NON_WORKING_DAY_NUMBER`, `NON_WORKING_DAY_CARRIER_FLAG`, `WEEKDAY`) VALUES\n";
+
+    public static final String INSERT_SUFFIX = "\nCOMMIT;";
+
+//    private int id;
+
     private String country;
 
     private String brand;
+
     private String calendarCode;
+
+    private int year;
+
+    private int totalWorkingDays;
+
+    private int totalNonWorkingDays;
+
+    private int month;
+
+    private LocalDate yearMonthDay;
+
+    private boolean nonWorkingDayFlag;
+
+    private int workingDayNumer;
+
+    private int nonWorkingDayNumer;
+
+    private boolean nonWorkingDayCarrierFlag;
+
+    private String weekday;
+
+    public Calendar(String country, String brand, String calendarCode, int year) {
+        this.country = country;
+        this.brand = brand;
+        this.calendarCode = calendarCode;
+        this.year = year;
+    }
+
+    public void setTotalWorkingDays(int totalWorkingDays) {
+        this.totalWorkingDays = totalWorkingDays;
+    }
+
+    public void setTotalNonWorkingDays(int totalNonWorkingDays) {
+        this.totalNonWorkingDays = totalNonWorkingDays;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYearMonthDay(LocalDate yearMonthDay) {
+        this.yearMonthDay = yearMonthDay;
+    }
+
+    public void setNonWorkingDayFlag(boolean nonWorkingDayFlag) {
+        this.nonWorkingDayFlag = nonWorkingDayFlag;
+    }
+
+    public void setWorkingDayNumer(int workingDayNumer) {
+        this.workingDayNumer = workingDayNumer;
+    }
+
+    public void setNonWorkingDayNumer(int nonWorkingDayNumer) {
+        this.nonWorkingDayNumer = nonWorkingDayNumer;
+    }
+
+    public void setNonWorkingDayCarrierFlag(boolean nonWorkingDayCarrierFlag) {
+        this.nonWorkingDayCarrierFlag = nonWorkingDayCarrierFlag;
+    }
+
+    public void setWeekday(String weekday) {
+        this.weekday = weekday;
+    }
+
+    @Override
+    public String toString() {
+        return "('" + country + "', '" + brand + "', '" + calendarCode + "', " + year + ", " + totalWorkingDays + ", "
+                + totalNonWorkingDays + ", " + month + ", '" + yearMonthDay + "', " + (nonWorkingDayFlag?1:0) + ", " + workingDayNumer
+                + ", " + nonWorkingDayNumer + ", " + (nonWorkingDayCarrierFlag?1:0) + ", " + weekday + ")";
+    }
 
 }
--- calendar_tool_20190104/src/main/java/jack/tools/calender/CalendarTool.java
+++ calendar_tool_20190104/src/main/java/jack/tools/calender/CalendarTool.java
@@ -1,5 +1,14 @@
 package jack.tools.calender;
 
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Theme:
  * <p>
@@ -9,5 +18,42 @@ package jack.tools.calender;
  * Created on 2019-01-04
  */
 public class CalendarTool {
+
+
+    public static List<Calendar> generateCalendar(String country, String brand, String calendarCode, int year, Set<LocalDate> nonWorkdingDays)
+            throws Exception {
+        if (StringUtils.isBlank(country) || StringUtils.isBlank(brand) || StringUtils.isBlank(calendarCode)
+                || year > LocalDate.now().getYear() + 1 || year < LocalDate.now().getYear()) {
+            throw new Exception("参数错误");
+        }
+
+        List<Calendar> calendars = new ArrayList<>();
+        int totalWorkingDays = Period.between(LocalDate.of(year, 1, 1), LocalDate.of(year-1, 1, 1)).getDays();
+        for (int i = 1; i < 365; i++) {
+            Calendar calendar = new Calendar(country, brand, calendarCode, year);
+            calendar.setTotalNonWorkingDays(totalWorkingDays);
+        }
+
+
+
+        return calendars;
+
+    }
+
+
+    public static void generateSql(List<Calendar> calendars, String path) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void test() {
+        int year = 2019;
+        int totalWorkingDays = Period.between(LocalDate.of(year-1, 1, 1), LocalDate.of(year, 1, 1)).getDays();
+        System.out.println(totalWorkingDays);
+    }
 
 }
