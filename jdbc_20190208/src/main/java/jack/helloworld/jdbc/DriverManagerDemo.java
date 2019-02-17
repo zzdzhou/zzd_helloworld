a547dab4ee92b6ce8c79112e0a1511d410b3b1ed
zzdzhou <zzd16000@163.com>
2019-02-17 11:55:41 +0000

    20190217

---
+++ jdbc_20190208/src/main/java/jack/helloworld/jdbc/DriverManagerDemo.java
@@ -0,0 +1,44 @@
+package jack.helloworld.jdbc;
+
+import java.sql.*;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2019-02-17
+ */
+public class DriverManagerDemo {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        String email = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmal?serverTimezone=UTC", "root", "zzde");
+            pstmt = con.prepareStatement("select email from user where id = ?");
+            pstmt.setInt(1, 3);
+            ResultSet resultSet = pstmt.executeQuery();
+            while (resultSet.next()) {
+                email = resultSet.getString("email");
+            }
+            System.out.println("log INFO - " + email);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- readme.sql
+++ readme.sql
@@ -1,3 +1,8 @@
--- jdbc_20190208 / ok / jdbc(DataSource) + JNDI + mysql + Jboss EAP 7.2 + Servlet
+-- jdbc_20190208 
+ok / jdbc(DataSource) + JNDI + mysql + Jboss EAP 7.2 + Servlet / jdbc(establish connection by DriverManager)
 
--- 
+-- jpa_applicationmanaged_entitymanager_20190217 
+no / mysql + jpa(application-managed EntityManager)
+
+
+
