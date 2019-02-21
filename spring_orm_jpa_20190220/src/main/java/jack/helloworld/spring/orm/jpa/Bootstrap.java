e6717f5d288ab18df6fb2527eb5addf288267b77
zzdzhou <zzd16000@163.com>
2019-02-21 00:07:31 +0000

    spring ioc

---
+++ spring_orm_jpa_20190220/src/main/java/jack/helloworld/spring/orm/jpa/Bootstrap.java
@@ -0,0 +1,13 @@
+package jack.helloworld.spring.orm.jpa;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2019-02-21
+ */
+public class Bootstrap {
+
+}
---
+++ spring_orm_jpa_20190220/src/main/java/jack/helloworld/spring/orm/jpa/context/SpringConfiguration.java
@@ -0,0 +1,17 @@
+package jack.helloworld.spring.orm.jpa.context;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2019-02-21
+ */
+public class SpringConfiguration {
+
+    public static void main(String[] args) {
+
+    }
+
+}
