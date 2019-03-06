3bdafdabbb8e17ae6cd772cbad679ae9a900de7f
zzdzhou <zzd16000@163.com>
2019-03-06 00:08:46 +0000

    use 2 DataSource with LocalContainerEntityManagerFactoryBean

---
+++ spring_orm_jpa_20190220/src/main/java/jack/helloworld/spring/orm/jpa/entity/remotedb/RUser.java
@@ -0,0 +1,72 @@
+package jack.helloworld.spring.orm.jpa.entity.remotedb;
+
+import javax.persistence.*;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2019-03-06
+ */
+@Entity
+public class RUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String email;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    private String username;
+
+    private String password;
+
+    public RUser() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ spring_orm_jpa_20190220/src/main/java/jack/helloworld/spring/orm/jpa/service/UserService.java
@@ -0,0 +1,17 @@
+package jack.helloworld.spring.orm.jpa.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2019-03-06
+ */
+@Service
+public class UserService {
+
+
+}
